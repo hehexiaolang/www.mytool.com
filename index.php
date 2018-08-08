576353f02851970b7cc81200f20c548529a3e41e
fenglang <1004859057@qq.com>
2018-08-09 00:38:47 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fenglang
+ * Date: 2018/8/8
+ * Time: 23:02
+ */
+?>
+
+<html>
+<head>
+    <title>我的常用工具</title>
+
+    <style type="text/css">
+        .item {
+            display: inline-block;
+            width: 128px;
+            height: 60px;
+            border: 2px solid antiquewhite;
+            text-align: center;
+            margin: 10px;
+            line-height: 60px;
+        }
+
+        .item a {
+            display: block;
+            width: 100%;
+            height: 100%;
+            text-decoration: none;
+            color: gray;
+        }
+    </style>
+</head>
+
+<body style="padding: 0;margin: 0;">
+<div style="width: 80%;margin: 80px auto 0 auto;text-align: center;">
+    <div>
+        <div class="item">
+            <a href="https://www.99cankao.com/colorconverter/color-coder.php" target="_blank"><span>RGB转换</span></a>
+        </div>
+        <div class="item">
+            <a href="https://tinypng.com/" target="_blank"><span>TinyPNG</span></a>
+        </div>
+    </div>
+
+    <div>
+        <div class="item">
+            <a href="https://cli.im/url" target="_blank"><span>草料二维码</span></a>
+        </div>
+        <div class="item">
+            <a href="http://dwz.cn/" target="_blank"><span>短网址生成</span></a>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
