3b481e33806399793568484f9b747877c0dfd47d
fenglang <1004859057@qq.com>
2018-10-17 22:09:42 +0800

    增加判断说明

--- shuangua.php
+++ shuangua.php
@@ -60,6 +60,15 @@ function gua()
                 $center_content .= "出错了";
         }
     }
+
+    $center_content .= "<br><h2>判断规则：</h2>"
+        . "第一种情况：有一个变爻，这个时候，就用本卦变爻的爻辞来判断吉凶。"
+        . "<br><br>第二种情况：有两个变爻，那就用本卦里这两个变爻的占辞来判断吉凶，并以位置靠上的那一个变爻的的占辞为主。"
+        . "<br><br>第三种情况：有三个变爻，就不能用变爻的爻辞来判断了，得用本卦和变卦的卦辞，以本卦的卦辞为主。"
+        . "<br><br>第四种情况：有四个变爻，这时就用变卦的两个不变爻的爻辞来判断吉凶。"
+        . "<br><br>第五种情况：有五个变爻，用变卦的那一个不变爻的爻辞来判断吉凶。"
+        . "<br><br>第六种情况：有六个变爻，这得分两种情况。一是六爻都是阳爻（乾卦），或者六爻都是阴爻（坤卦），那么，如果是乾卦，就用乾卦“用九”的爻辞判断吉凶，如果是坤卦，就用坤卦“用六”的爻辞判断吉凶。二是除了这两种情况之外的其他六爻全变的情况，就用变卦的卦辞来判断吉凶。"
+        . "<br><br>第七种情况：六爻一个都没变，这时用本卦的卦辞来判断吉凶。";
 }
 
 function yao()
@@ -279,10 +288,10 @@ function get_detail_url($yao_arr)
     <div class="frame">
         <?= $left_content ?>
     </div>
-    <div class="frame" style="margin: 150px auto auto 200px;">
+    <div class="frame" style="margin: auto auto auto 150px;width: 400px">
         <?= $center_content ?>
     </div>
-    <div class="frame" style="margin-left: 200px;width: 400px">
+    <div class="frame" style="margin-left: 150px;width: 400px">
         <?= $right_content ?>
     </div>
 </div>
