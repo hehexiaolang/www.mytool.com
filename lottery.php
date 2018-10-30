e13a559ed8a496bc9af51fb417783bb14377db3a
fenglang <1004859057@qq.com>
2018-10-30 19:23:43 +0800

    增加最多

--- lottery.php
+++ lottery.php
@@ -10,11 +10,9 @@ header("content-type:text/html;charset=utf-8");
 $html = file_get_contents("https://m.500.com/info/kaijiang/moreexpect/ssq/?0_ala_h5baidu");
 
 $head_arr = [];
-$head_avg = 0;
 $head_cnt = 0;
 
 $tail_arr = [];
-$tail_avg = 0;
 $tail_cnt = 0;
 
 if (preg_match_all("/kaij-jg-wq[\s\S]+?\/ul/", $html, $ret)) {
@@ -34,29 +32,40 @@ if (preg_match_all("/kaij-jg-wq[\s\S]+?\/ul/", $html, $ret)) {
     }
 }
 
+$head_max = max($head_arr);
 $head_avg = number_format($head_cnt / 33, 2);
+
+$tail_max = max($tail_arr);
 $tail_avg = $tail_cnt / 16;
 
 /** 结果显示 **/
-$head_content = "红区:<br>平均次数：$head_cnt / 33 = <font color='red'>$head_avg</font><br>";
+$head_content = "红区:<br>平均次数：$head_cnt / 33 = <font color='green'>$head_avg</font>";
 
 foreach ($head_arr as $key => $value) {
     if ($value >= $head_avg) {
-        $head_content .= "<font size='2'>&nbsp;&nbsp;&nbsp;&nbsp;[$key] => $value</font>"
-            . "<font size='2' color='red'>&nbsp;&nbsp;&nbsp;&nbsp;建议</font><br>";
+        $head_content .= "<br><font size='2'>&nbsp;&nbsp;&nbsp;&nbsp;[$key] => $value</font>"
+            . "<font size='2' color='green'>&nbsp;&nbsp;&nbsp;&nbsp;建议</font>";
+
+        if ($value == $head_max) {
+            $head_content .= "<font size='2' color='red'>&nbsp;&nbsp;&nbsp;&nbsp;最多</font>";
+        }
     } else {
-        $head_content .= "<font size='2'>&nbsp;&nbsp;&nbsp;&nbsp;[$key] => $value</font><br>";
+        $head_content .= "<br><font size='2'>&nbsp;&nbsp;&nbsp;&nbsp;[$key] => $value</font>";
     }
 }
 
-$tail_content = "蓝区:<br>平均次数：$tail_cnt / 16 = <font color='red'>$tail_avg</font><br>";
+$tail_content = "蓝区:<br>平均次数：$tail_cnt / 16 = <font color='green'>$tail_avg</font>";
 
 foreach ($tail_arr as $key => $value) {
     if ($value >= $tail_avg) {
-        $tail_content .= "<font size='2'>&nbsp;&nbsp;&nbsp;&nbsp;[$key] => $value</font>"
-            . "<font size='2' color='red'>&nbsp;&nbsp;&nbsp;&nbsp;建议</font><br>";
+        $tail_content .= "<br><font size='2'>&nbsp;&nbsp;&nbsp;&nbsp;[$key] => $value</font>"
+            . "<font size='2' color='green'>&nbsp;&nbsp;&nbsp;&nbsp;建议</font>";
+
+        if ($value == $tail_max) {
+            $tail_content .= "<font size='2' color='red'>&nbsp;&nbsp;&nbsp;&nbsp;最多</font>";
+        }
     } else {
-        $tail_content .= "<font size='2'>&nbsp;&nbsp;&nbsp;&nbsp;[$key] => $value</font><br>";
+        $tail_content .= "<br><font size='2'>&nbsp;&nbsp;&nbsp;&nbsp;[$key] => $value</font>";
     }
 }
 ?>
